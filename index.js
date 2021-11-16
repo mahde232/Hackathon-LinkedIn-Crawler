ed8d480bc4425e6028d81f530f5179e41cf74b4a
mahde232 <49252163+mahde232@users.noreply.github.com>
2021-11-16 18:40:02 +0200

    added working version of GetDataFromSpecificProfile function

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
-const axios = require('axios')
 
-
-async function main() {
+const GetDataFromSpecificProfile = async () => {
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: { width: 1920, height: 1080 }
@@ -11,40 +9,46 @@ async function main() {
     const page = await browser.newPage();
 
     await page.goto("https://www.linkedin.com/uas/login")
-    await page.type('#username', 'kesaxo2766@incoware.com');
-    await page.type('#password', '123456mahdeshadi');
+    await page.type('#username', 'jebigom876@idrct.com');
+    await page.type('#password', 'asdfg12345');
     await page.waitForSelector('.btn__primary--large');
     await page.click('.btn__primary--large');
     await page.waitFor(3000);
-    await page.goto('https://www.linkedin.com/in/amir-torgeman-32566b226/');
-    await page.waitForSelector('.pb2, .pv-text-details__separator, .link-without-visited-state');
-    await page.click('.pb2, .pv-text-details__separator, .link-without-visited-state');
-
-    // let data = await page.evaluate(() => document.querySelector('*').outerHTML); //get all page html so we can scrape it using cheerio
-    // let $ = cheerio.load(data)
-    // const personName = $('.text-heading-xlarge').text();
-    // const personNameFromH1 = $('h1').text();
-    // console.log('personName=',personName);
-    // console.log('personNameFromH1=',personNameFromH1);
-
-
-
-    // $('.pb2').each((i,element)=>{
-    //     const location = $(element).find('.text-body-small').text();
-    //     console.log('location=',location);
-    // })
-
-    //class="pv-contact-info__contact-type ci-email"
-    // $('.pv-contact-info__contact-type .ci-email, li-icon').next().next().find('a').each((i,element)=>{
-    //     console.log('element index=',i);
-    //     const emailTagText = $(element).find('a').text() //class="pv-contact-info__contact-link link-without-visited-state t-14"
-    //     const emailTagHref = $(element).find('a').attr('href');
-    //     console.log('emailTagText',emailTagText);
-    //     console.log('emailTagHref',emailTagHref);
-    // })
-
+    await page.goto('https://www.linkedin.com/in/shadi-masood-b1571a226/');;
+    //get all page html so we can scrape it using cheerio
+    let data = await page.evaluate(() => document.querySelector('*').outerHTML); 
+    let $ = cheerio.load(data)
+    //grab name
+    const personName = $('.text-heading-xlarge').text().trim();
+    console.log('personName=',personName);
+    //grab location
+    $('.pb2.pv-text-details__left-panel').each((i,element)=>{
+        const location = $(element).find('.text-body-small.inline.t-black--light.break-words').text().trim();
+        console.log('location=',location);
+    })
+    //go to email page
+    await page.goto('https://www.linkedin.com/in/shadi-masood-b1571a226/detail/contact-info/')
+    data = await page.evaluate(() => document.querySelector('*').outerHTML); //get all page html so we can scrape it using cheerio
+    $ = cheerio.load(data)
+    let email = '';
+    let profileLink = '';
+    $('.pv-contact-info__contact-link.link-without-visited-state.t-14').each((i,element)=> {
+        if(i === 0)
+            profileLink = $(element).text().trim();
+        if(i === 1)
+            email = $(element).text().trim();
+    })
+    console.log('email=',email);
+    console.log('profileLink=',profileLink);
+    console.log('');
     console.log('Done scraping page');
-    // await browser.close();
+    console.log('---------------');
+    return {
+        name: personName,
+        location: location,
+        email: email,
+        profileLink: profileLink
+    }
 }
 
-main();
+GetDataFromSpecificProfile();

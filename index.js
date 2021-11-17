d8ac3c135ee11b7f900a1c48a2b93c08712bd94e
Shadi Rada <shakkyr@gmail.com>
2021-11-17 13:56:56 +0200

    almost final version

--- index.js
+++ index.js
@@ -1,132 +1,166 @@
-const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
-const fs = require('fs');
-const mongoose = require('mongoose');
-const profileModel = require('./models/profiles.model').profilesModel;
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const mongoose = require("mongoose");
+const profileModel = require("./models/profiles.model").profilesModel;
 //puppeteer params
 const loginToLinkedInFakeAccount = async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: { width: 1920, height: 1080 }
-    });          //! when running puppeteer in heroku/AWS or similar, headless shoud be set tp true
-    const page = await browser.newPage();
-    await page.goto("https://www.linkedin.com/uas/login")
-    await page.type('#username', 'sitedo7204@idrct.com');
-    await page.type('#password', 'asdfg12345');
-    await page.waitForSelector('.btn__primary--large');
-    await page.click('.btn__primary--large');
-    await page.waitFor(15000);
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: { width: 1920, height: 1080 },
+  }); //! when running puppeteer in heroku/AWS or similar, headless shoud be set tp true
+  const page = await browser.newPage();
+  await page.goto("https://www.linkedin.com/uas/login");
+  await page.type("#username", "dibewa@thichanthit.com");
+  await page.type("#password", "shakky17");
+  await page.waitForSelector(".btn__primary--large");
+  await page.click(".btn__primary--large");
+  await page.waitFor(15000);
 
-    const resultsFromLinkedIn = await linkedInSearchProfiles(page, 2);
-    console.log('From Shadi Function=', resultsFromLinkedIn);
-    const resultsFromGoogle = await googleSearchProfiles(page, 3)
-    console.log('From Saleh Function=', resultsFromGoogle);
+  const resultsFromLinkedIn = await linkedInSearchProfiles(page, 2);
+  console.log("From Shadi Function=", resultsFromLinkedIn);
+  const resultsFromGoogle = await googleSearchProfiles(page, 3);
+  console.log("From Saleh Function=", resultsFromGoogle);
 
-    let arrayOfResult = [];
-    for (const profileLink of resultsFromGoogle) {
-        console.log(`Crawling Link=${profileLink}`);
-        const dataResultObj = await GetDataFromSpecificProfile(page, profileLink);
-        console.log('dataResultObj=', dataResultObj);
-        arrayOfResult.push(dataResultObj);
-    }
-    // resultsFromGoogle.forEach(async (profileLink,index) => {
-    //     console.log(`Crawling Link=${profileLink} with index=${index}`);
-    //     const dataResultObj = await GetDataFromSpecificProfile(page, profileLink);
-    //     console.log('dataResultObj=',dataResultObj);
-    // })
+  let arrayOfResult = [];
+  for (const profileLink of resultsFromGoogle) {
+    console.log(`Crawling Link=${profileLink}`);
+    const dataResultObj = await GetDataFromSpecificProfile(page, profileLink);
+    console.log("dataResultObj=", dataResultObj);
+    arrayOfResult.push(dataResultObj);
+  }
+  // resultsFromGoogle.forEach(async (profileLink,index) => {
+  //     console.log(`Crawling Link=${profileLink} with index=${index}`);
+  //     const dataResultObj = await GetDataFromSpecificProfile(page, profileLink);
+  //     console.log('dataResultObj=',dataResultObj);
+  // })
 
-    resultsFromLinkedIn.forEach(async (profileLink) => {
-        console.log(`Crawling Link=${profileLink}`);
-        const dataResultObj = await GetDataFromSpecificProfile(page,profileLink);
-        console.log('dataResultObj=', dataResultObj);
-        arrayOfResult.push(dataResultObj);
+  resultsFromLinkedIn.forEach(async (profileLink) => {
+    console.log(`Crawling Link=${profileLink}`);
+    const dataResultObj = await GetDataFromSpecificProfile(page, profileLink);
+    console.log("dataResultObj=", dataResultObj);
+    arrayOfResult.push(dataResultObj);
+  });
+  console.log("arrayOfResult", arrayOfResult);
+  //Save arrayOfResults to the DB using InsertMany (could be a function, could be in this code)
+  profileModel
+    .insertMany(arrayOfResult)
+    .then(() => {
+      console.log("Inserted to DB");
     })
-    console.log('arrayOfResult: ', arrayOfResult);
-    //Save arrayOfResults to the DB using InsertMany (could be a function, could be in this code)
-    profileModel.insertMany(arrayOfResult).then(() => {
-        console.log('inserted');
-    }).catch(err => {
-        console.log(err);
-    })
-}
+    .catch((err) => {
+      console.log("error=", err);
+    });
+};
 
 const GetDataFromSpecificProfile = async (page, profile) => {
-    await page.goto(`${profile}`, { waitUntil: 'networkidle2' });
-    await page.waitForSelector('.pb2.pv-text-details__left-panel a.link-without-visited-state')
-    await page.click('.pb2.pv-text-details__left-panel a.link-without-visited-state')
+  await page.goto(`${profile}`, { waitUntil: "networkidle2" });
+  //get all page html so we can scrape it using cheerio
+  await page.waitForSelector(
+    ".pb2.pv-text-details__left-panel a.link-without-visited-state"
+  );
+  await page.click(
+    ".pb2.pv-text-details__left-panel a.link-without-visited-state"
+  );
 
-    const puppeteerData = await page.evaluate((profile) => {
-        const personName = document.querySelector('.text-heading-xlarge').innerText.trim();
-        const location = document.querySelector('.pb2.pv-text-details__left-panel .text-body-small').innerText.trim();
-        //TODO =>
-        // const email = document.querySelector('.pv-contact-info__ci-container a').innerText; //THIS SHIT DOESNT WORK, ASK FOR HELP
-        return {
-            name: personName || 'name-not-found',
-            location: location || 'location-not-found',
-            // email: email || 'no-public-email-available',
-        };
-    })
-    puppeteerData['profileLink'] = profile;
-    return puppeteerData;
-}
+  const puppeteerData = await page.evaluate(() => {
+    const personName = document
+      .querySelector(".text-heading-xlarge")
+      .innerText.trim();
+    const location = document
+      .querySelector(".pb2.pv-text-details__left-panel .text-body-small")
+      .innerText.trim();
+    // const email = document.querySelector('.pv-contact-info__ci-container a').innerText; //THIS SHIT DOESNT WORK, ASK FOR HELP
+    return {
+      name: personName || "name-not-found",
+      location: location || "location-not-found",
+      // email: email || 'no-public-email-available',
+      profileLink: profile,
+    };
+  });
+  return puppeteerData;
+};
 
 //Shadi
 const linkedInSearchProfiles = async (page, howManyPages) => {
-    let linksArray = [];
-    const titles = ['ceo', 'cto', 'co-founder'];
-    let searchQueryURL = 'https://www.linkedin.com/search/results/people/?geoUrn=%5B%22101620260%22%5D&keywords=stealth%20startup';
-    for (let i = 1; i <= howManyPages; i++) {
+  let linksArray = [];
+  const titles = ["ceo", "cto", "co-founder"];
+  let searchQueryURL =
+    "https://www.linkedin.com/search/results/people/?geoUrn=%5B%22101620260%22%5D&keywords=stealth%20startup";
+  for (let i = 1; i <= howManyPages; i++) {
+    console.log(`${searchQueryURL}&title=${titles[0]}`);
+    await page.goto(`${searchQueryURL}&title=${titles[0]}`);
+    let htmlContent = await page.content();
+    let $ = cheerio.load(htmlContent);
+    let newLinksToVisit = $(".app-aware-link")
+      .map((index, element) => $(element).attr("href"))
+      .get(); //get links to profiles from search
+    linksArray = [...linksArray, ...newLinksToVisit];
 
-        console.log(`${searchQueryURL}&title=${titles[0]}`);
-        await page.goto(`${searchQueryURL}&title=${titles[0]}`)
-        let htmlContent = await page.content();
-        let $ = cheerio.load(htmlContent);
-        let newLinksToVisit = $(".app-aware-link").map((index, element) => $(element).attr("href")).get(); //get links to profiles from search
-        linksArray = [...linksArray, ...newLinksToVisit];
+    console.log(`${searchQueryURL}&title=${titles[1]}`);
+    await page.goto(`${searchQueryURL}&title=${titles[1]}`);
+    htmlContent = await page.content();
+    $ = cheerio.load(htmlContent);
+    newLinksToVisit = $(".app-aware-link")
+      .map((index, element) => $(element).attr("href"))
+      .get(); //get links to profiles from search
+    linksArray = [...linksArray, ...newLinksToVisit];
 
-        console.log(`${searchQueryURL}&title=${titles[1]}`);
-        await page.goto(`${searchQueryURL}&title=${titles[1]}`)
-        htmlContent = await page.content();
-        $ = cheerio.load(htmlContent);
-        newLinksToVisit = $(".app-aware-link").map((index, element) => $(element).attr("href")).get(); //get links to profiles from search
-        linksArray = [...linksArray, ...newLinksToVisit];
+    console.log(`${searchQueryURL}&title=${titles[2]}`);
+    await page.goto(`${searchQueryURL}&title=${titles[2]}`);
+    htmlContent = await page.content();
+    $ = cheerio.load(htmlContent);
+    newLinksToVisit = $(".app-aware-link")
+      .map((index, element) => $(element).attr("href"))
+      .get(); //get links to profiles from search
+    linksArray = [...linksArray, ...newLinksToVisit];
 
-        console.log(`${searchQueryURL}&title=${titles[2]}`);
-        await page.goto(`${searchQueryURL}&title=${titles[2]}`)
-        htmlContent = await page.content();
-        $ = cheerio.load(htmlContent);
-        newLinksToVisit = $(".app-aware-link").map((index, element) => $(element).attr("href")).get(); //get links to profiles from search
-        linksArray = [...linksArray, ...newLinksToVisit];
-
-        searchQueryURL = `${searchQueryURL}&page=${i + 1}`;
-        await page.goto(`${searchQueryURL}`);
-    }
-    fs.writeFileSync('./linkedInProfiles.json', JSON.stringify(linksArray));
-    return linksArray;
-}
+    searchQueryURL = `${searchQueryURL}&page=${i + 1}`;
+    await page.goto(`${searchQueryURL}`);
+  }
+  const uniq = [...new Set(linksArray)];
+  console.log(uniq);
+  const filteredData = uniq.filter((ele) => {
+    console.log(ele.substring(24).split("/")[0]);
+    if (ele.substring(24).split("/")[0] === "in") return true;
+    else return false;
+  });
+  console.log("filteredData:", filteredData);
+  fs.writeFileSync("./linkedInProfiles.json", JSON.stringify(filteredData));
+  return filteredData;
+};
 
 //Saleh
 const googleSearchProfiles = async (page, howManyPages) => {
-    const profiles = [];
-    await page.goto("https://www.google.com/search?q=linkedIn+Israeli+stealth+startup+ceo+cto+co-founder");
-    for (let i = 1; i <= howManyPages; i++) {
-        let data = await page.evaluate(() => document.querySelector('*').outerHTML); //get all page html so we can scrape it using cheerio
-        let $ = cheerio.load(data)
-        $('.yuRUbf').each((i, element) => {
-            const profileUrl = $(element).find('a').attr('href');
-            if (profileUrl.substring(24).split('/')[0] === 'in')
-                profiles.push(profileUrl);
-        });
-        await page.goto(`https://www.google.com/search?q=linkedIn+Israeli+stealth+startup+ceo+cto+co-founder&start=${i * 10}`);
-    }
+  const profiles = [];
+  await page.goto(
+    "https://www.google.com/search?q=linkedIn+Israeli+stealth+startup+ceo+cto+co-founder"
+  );
+  for (let i = 1; i <= howManyPages; i++) {
+    let data = await page.evaluate(() => document.querySelector("*").outerHTML); //get all page html so we can scrape it using cheerio
+    let $ = cheerio.load(data);
+    $(".yuRUbf").each((i, element) => {
+      const profileUrl = $(element).find("a").attr("href");
+      if (profileUrl.substring(24).split("/")[0] === "in")
+        profiles.push(profileUrl);
+    });
+    await page.goto(
+      `https://www.google.com/search?q=linkedIn+Israeli+stealth+startup+ceo+cto+co-founder&start=${
+        i * 10
+      }`
+    );
+  }
 
-    fs.writeFileSync('./googleProfiles.json', JSON.stringify(profiles));
-    return profiles;
-}
-
+  fs.writeFileSync("./googleProfiles.json", JSON.stringify(profiles));
+  return profiles;
+};
 
 loginToLinkedInFakeAccount();
 
-mongoose.connect('mongodb+srv://mahde:asdfg12345@cluster0.tdz2o.mongodb.net/LinkedInDB?retryWrites=true&w=majority', { useNewUrlParser: true }, () => {
-    console.log('connected to db');
-});
+mongoose.connect(
+  "mongodb+srv://mahde:asdfg12345@cluster0.tdz2o.mongodb.net/LinkedInDB?retryWrites=true&w=majority",
+  { useNewUrlParser: true },
+  () => {
+    console.log("connected to db");
+  }
+);
